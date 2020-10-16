542435a62f492fd53cc9a71bdee5948c33fe0611
LastCow9000 <sys19912002@hanmail.net>
2020-10-16 12:15:19 +0900

    풀 받기 위한 커밋

--- src/HoH/controller/getPostListByAgeController.java
+++ src/HoH/controller/getPostListByAgeController.java
@@ -6,24 +6,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import HoH.model.BoardDAO;
+import HoH.model.ListVO;
+import HoH.model.PagingBean;
 import HoH.model.PostVO;
-// 시대별 게시물 받아오는 기능
+
 public class getPostListByAgeController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String ageName = request.getParameter("agename");
+		int totalPostCount = BoardDAO.getInstance().getTotalPostCount(ageName);
+		String pageNo = request.getParameter("pageNo");
+		PagingBean pagingBean = null;
 		ArrayList<PostVO> list = new ArrayList<PostVO>();
-		if(ageName.contentEquals("고조선시대")) {
-			list = BoardDAO.getInstance().getListByAge(ageName);
-		} else if(ageName.contentEquals("삼국시대")) {
-			list = BoardDAO.getInstance().getListByAge(ageName);
-		} else if(ageName.contentEquals("고려시대")) {
-			list = BoardDAO.getInstance().getListByAge(ageName);
-		} else if(ageName.contentEquals("조선시대")) {
-			list = BoardDAO.getInstance().getListByAge(ageName);
+		if(pageNo==null) {
+			pagingBean = new PagingBean(totalPostCount);
+		} else {
+			pagingBean = new PagingBean(totalPostCount,Integer.parseInt(pageNo));
 		}
-		request.setAttribute("list", list);
+		ListVO lvo=new ListVO(list, pagingBean);
+		if(ageName.contentEquals("고조선시대")) {
+			list = BoardDAO.getInstance().getListByAge(ageName,pagingBean);
+			lvo.setList(list);
+		} else if(ageName.contentEquals("삼국시대")) {
+			list = BoardDAO.getInstance().getListByAge(ageName,pagingBean);
+			lvo.setList(list);
+		} else if(ageName.contentEquals("고려시대")) {
+			list = BoardDAO.getInstance().getListByAge(ageName , pagingBean);
+			lvo.setList(list);
+		} else if(ageName.contentEquals("조선시대")) {
+			list = BoardDAO.getInstance().getListByAge(ageName, pagingBean);
+			lvo.setList(list);
+		}
+		
+		request.setAttribute("listvo", lvo);
 		request.setAttribute("ageName", ageName);
 		request.setAttribute("url", "/board/postListByAge.jsp");
 		return "/template/layout.jsp";
